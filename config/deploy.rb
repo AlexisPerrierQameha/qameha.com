9a6b26e830a0b06fc670169d8969f51b44f8c346
Darjeelink <alexis.perrier@gmail.com>
2010-01-14 06:25:02 +0100

    2.3.5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,38 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+#before "deploy:symlink", :ln_folders
+#after "deploy:symlink" , :copy_database_yml
+#after "deploy:symlink", "deploy:update_crontab"
+#after "deploy", "deploy:cleanup"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+#require 'capistrano/ext/multistage'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, 'qameha'
+set :user, 'spock'
+set :git_account, 'darjeelink'
+set :scm_passphrase, "galactus88260" #This is your custom users password
+default_run_options[:pty] = true
+set :repository,  "git@github.com:darjeelink/qameha.com.git"
+set :scm, "git"
+set :user, "spock"
+ssh_options[:forward_agent] = true
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+set :use_sudo, false
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+set :server_hostname, 'qameha.com'
+set :location, "204.232.206.121"
+set :rails_env, "production"
+set :deploy_to, "/home/spock/qameha.com/"
+set :port, 4800
+
+role :app, location
+role :web, location
+role :db,  location, :primary => true
